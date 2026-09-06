b3062b9e89287c06ff9694be806df01f44d34e08
Nicolas <wpiedrabuena@gmail.com>
2026-09-06 10:34:25 -0300

    -

--- api/index.php
+++ api/index.php
@@ -2,15 +2,31 @@
 
 declare(strict_types=1);
 
-$_ENV['APP_CONFIG_CACHE'] ??= '/tmp/config.php';
-$_ENV['APP_EVENTS_CACHE'] ??= '/tmp/events.php';
-$_ENV['APP_PACKAGES_CACHE'] ??= '/tmp/packages.php';
-$_ENV['APP_ROUTES_CACHE'] ??= '/tmp/routes.php';
-$_ENV['APP_SERVICES_CACHE'] ??= '/tmp/services.php';
-$_ENV['VIEW_COMPILED_PATH'] ??= '/tmp/views';
+$runtimeDefaults = [
+    'APP_CONFIG_CACHE' => '/tmp/config.php',
+    'APP_EVENTS_CACHE' => '/tmp/events.php',
+    'APP_PACKAGES_CACHE' => '/tmp/packages.php',
+    'APP_ROUTES_CACHE' => '/tmp/routes.php',
+    'APP_SERVICES_CACHE' => '/tmp/services.php',
+    'VIEW_COMPILED_PATH' => '/tmp/views',
+    'LOG_CHANNEL' => 'stderr',
+    'SESSION_DRIVER' => 'cookie',
+    'CACHE_STORE' => 'array',
+    'QUEUE_CONNECTION' => 'sync',
+];
 
-foreach ($_ENV as $key => $value) {
-    if (is_scalar($value)) {
+foreach ($runtimeDefaults as $key => $value) {
+    if (getenv($key) === false && ! array_key_exists($key, $_ENV) && ! array_key_exists($key, $_SERVER)) {
+        $_ENV[$key] = $value;
+    }
+}
+
+foreach (array_replace($_SERVER, $_ENV) as $key => $value) {
+    if (
+        is_string($key)
+        && is_scalar($value)
+        && preg_match('/^(APP|DB|SESSION|CACHE|QUEUE|LOG|MAIL|BROADCAST|FILESYSTEM|AWS|REDIS|MEMCACHED|VITE)_/', $key) === 1
+    ) {
         putenv($key.'='.$value);
     }
 }
